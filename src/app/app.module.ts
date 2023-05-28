d5c91de77c89f19efcd7d1ec3f4000cd9380e0e9
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-05-29 02:06:01 +0300

    finishng home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,39 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProjectComponent } from './components/project/project.component';
-import { FeaturedProjectsComponent } from './components/featured-projects/featured-projects.component';
-import { CategoryComponent } from './components/category/category.component';
-import { CategoriesComponent } from './components/categories/categories.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { HighestProjectsComponent } from './components/highest-projects/highest-projects.component';
-import { ProjectCardComponent } from './components/project-card/project-card.component';
+
 import { SearchComponent } from './components/search/search.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProjectItemComponent } from './components/project-item/project-item.component';
-import { LandingComponent } from './components/landing/landing.component';
+
 import { FooterComponent } from './components/footer/footer.component';
+import { ProjectModule } from './project.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    ProjectComponent,
-    FeaturedProjectsComponent,
-    CategoryComponent,
-    CategoriesComponent,
-    ProjectsComponent,
-    HighestProjectsComponent,
-    ProjectCardComponent,
-    SearchComponent,
-    ProjectItemComponent,
-    LandingComponent,
-    FooterComponent
+
   ],
   imports: [
     BrowserModule,
+    ProjectModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- src/app/project.module.ts
+++ src/app/project.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ProjectComponent } from './components/project/project.component';
 import { HomeComponent } from './components/home/home.component';
+import { FeaturedProjectsComponent } from './components/featured-projects/featured-projects.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { HighestProjectsComponent } from './components/highest-projects/highest-projects.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { ProjectItemComponent } from './components/project-item/project-item.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 const routes: Routes = [
@@ -17,9 +28,25 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+HomeComponent,
+    ProjectComponent,
+    LandingComponent,
+    FeaturedProjectsComponent,
+    CategoryComponent,
+    CategoriesComponent,
+    ProjectsComponent,
+    ProjectCardComponent,
+    HighestProjectsComponent,
+    ProjectCardComponent,
+    ProjectItemComponent,
+    NavbarComponent,
+    SearchComponent,
+    FooterComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes),
   ]
 })
 export class ProjectModule { }
