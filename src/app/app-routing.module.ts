9c37f567bc364fc993d67a366654be0add04e93f
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-06-01 17:36:35 +0300

    email verification route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { EmailConfirmationComponent } from './components/email-confirmation/email-confirmation.component';
 
 const routes: Routes = [
   { path: 'home', component:HomeComponent  },
   {path : 'register', component:RegisterComponent},
+  {path : 'verify-email/{email}', component:EmailConfirmationComponent},
   {path:'login',component:LoginComponent}
 ];
 
